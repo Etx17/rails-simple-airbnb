13f6c406b172e95fad31d1762704bcd27dea5eb1
Etienne de Dianous <etiennededi@hotmail.fr>
2022-04-19 11:14:09 +0200

    Index done, need some slight visual fixes and navbar to work on

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Huge penthouse with big sauna',
+  address: '3 rue des batignolles 75017',
+  description: "La cuisine est si large qu'on peut y faire entrer un cachalot. Pareil pour le sauna, la salle de bain et les chambres des enfants. Idéal pour se reposer un week end",
+  price_per_night: 175,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'TIny shack on a frozen lake',
+  address: 'Into the wild, Arizona, W9 1DT',
+  description: 'A lovely summer feel, ground yourself and keep in touch with nature in this wooden shack. Wolves are said to be living around here. Dont panic, they are nice.',
+  price_per_night: 25,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Chambre étudiante tavu ',
+  address: '10 bd Barbès',
+  description: 'La drogue et la criminalité ont envahi le quartier. Vous ne vous sentirez pas en sécurité dans coin de Paris atypique. Pour ceux qui recherchent des sensations fortes',
+  price_per_night: 32,
+  number_of_guests: 2
+)
